fe96cefecd73a95510e560d8439e075a927e5367
Dominik Stadler <dominik.stadler@gmx.at>
2016-10-25 10:04:44 +0200

    Rework sorting to properly handle more cases that we see in the found data

--- src/main/java/org/dstadler/github/VersionComparator.java
+++ src/main/java/org/dstadler/github/VersionComparator.java
@@ -1,80 +1,163 @@
 package org.dstadler.github;
 
+import com.google.common.base.Preconditions;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.regex.Pattern;
 
 public class VersionComparator implements Comparator<String> {
     private final static Pattern SIMPLE_VERSION = Pattern.compile("[0-9.][0-9.]+[0-9.]");
-    private final static Pattern BETA_VERSION = Pattern.compile("[0-9.][0-9.]+[0-9.]-beta\\d+");
+    private final static Pattern BETA_VERSION = Pattern.compile("[0-9.][0-9.]+[0-9.]-(?:beta|BETA|SNAPSHOT|bbn)\\d*(-\\d*)?");
+
+    public static class Version implements Comparable<Version> {
+        public int major;
+        public int minor;
+        public int revision;
+        public boolean beta;
+        public boolean snapshot;
+        public boolean other;
+        public int betaSnapshotOther;
+
+        public static Version parse(String version) {
+            Version v = new Version();
+
+            // sort these as highest version for now
+            if(version.equals("other")) {
+                v.major = 99;
+                return v;
+            }
+
+            String[] parts = version.split("[-.]");
+            Preconditions.checkState(parts.length > 1 && parts.length < 5, "Need a version with 2 to 4 parts, but had %s resulting from %s", Arrays.toString(parts), version);
+
+            v.major = Integer.parseInt(parts[0]);
+            v.minor = Integer.parseInt(parts[1]);
+            if(parts.length > 2) {
+                handleBetaSnapshortOrRevision(v, parts[2]);
+            }
+            if(parts.length > 3) {
+                if(!v.beta && !v.snapshot && !v.other) {
+                    handleBetaSnapshortOrRevision(v, parts[3]);
+                } else if (v.betaSnapshotOther == 0) {
+                    v.betaSnapshotOther = Integer.parseInt(parts[3]);
+                }
+            }
+
+            return v;
+        }
+
+        private static void handleBetaSnapshortOrRevision(Version v, String part) {
+            if (isBeta(part)) {
+                v.beta = true;
+                part = StringUtils.removeStartIgnoreCase(part, "beta");
+                if(StringUtils.isNotEmpty(part)) {
+                    v.betaSnapshotOther = Integer.parseInt(part);
+                }
+            } else if (isSnapshot(part)) {
+                v.snapshot = true;
+                part = StringUtils.removeStartIgnoreCase(part, "snapshot");
+                if(StringUtils.isNotEmpty(part)) {
+                    v.betaSnapshotOther = Integer.parseInt(part);
+                }
+            } else {
+                try {
+                    v.revision = Integer.parseInt(part);
+                } catch (NumberFormatException e) {
+                    v.other = true;
+                }
+            }
+        }
+
+        @Override
+        public int compareTo(Version o) {
+            if(major != o.major) {
+                return Integer.compare(major, o.major);
+            }
+            if(minor != o.minor) {
+                return Integer.compare(minor, o.minor);
+            }
+            if(revision != o.revision) {
+                return Integer.compare(revision, o.revision);
+            }
+            if(beta && !o.beta) {
+                if(o.snapshot || o.other) {
+                    return Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                } else {
+                    return -1*Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                }
+            }
+            if(!beta && o.beta) {
+                if(snapshot || other) {
+                    return Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                } else {
+                    return -1*Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                }
+            }
+            if(snapshot && !o.snapshot) {
+                if(o.other) {
+                    return Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                } else {
+                    if(betaSnapshotOther == o.betaSnapshotOther) {
+                        return -1;
+                    }
+
+                    return -1*Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                }
+            }
+            if(!snapshot && o.snapshot) {
+                if(other) {
+                    return Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                } else {
+                    if(betaSnapshotOther == o.betaSnapshotOther) {
+                        return 1;
+                    }
+
+                    return -1*Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+                }
+            }
+
+            return Integer.compare(betaSnapshotOther, o.betaSnapshotOther);
+        }
+
+        @Override
+        public String toString() {
+            return "Version{" +
+                    "major=" + major +
+                    ", minor=" + minor +
+                    ", revision=" + revision +
+                    ", beta=" + beta +
+                    ", snapshot=" + snapshot +
+                    ", other=" + other +
+                    ", betaSnapshotOther=" + betaSnapshotOther +
+                    '}';
+        }
+    }
 
     @Override
     public final int compare(String var1, String var2) {
         boolean simple1 = var1 != null && SIMPLE_VERSION.matcher(var1).matches();
         boolean simple2 = var2 != null && SIMPLE_VERSION.matcher(var2).matches();
 
-        // check for beta-versions
+        // check for beta/snapshot-versions
         boolean beta1 = var1 != null && BETA_VERSION.matcher(var1).matches();
         boolean beta2 = var2 != null && BETA_VERSION.matcher(var2).matches();
 
-        if((simple1 || beta1) && (simple2 || beta2)) {
-            String[] parts1 = var1.split("[-.]");
-            String[] parts2 = var2.split("[-.]");
+        if(("other".equals(var1) || simple1 || beta1) && ("other".equals(var2) || simple2 || beta2)) {
+            Version v1 = Version.parse(var1);
+            Version v2 = Version.parse(var2);
 
-            // check for non-beta-versions
-            for (int i = 0; i < parts1.length && i < parts2.length; i++) {
-                String part1 = parts1[i];
-                String part2 = parts2[i];
+            int ret = v1.compareTo(v2);
 
-                boolean betaPos1 = false;
-                if(part1.startsWith("beta")) {
-                    part1 = StringUtils.removeStart(part1, "beta");
-                    betaPos1 = true;
-                }
-
-                boolean betaPos2 = false;
-                if(part2.startsWith("beta")) {
-                    part2 = StringUtils.removeStart(part2, "beta");
-                    betaPos2 = true;
-                }
-
-                // handle cases of different beta-ness
-                if (betaPos1 && !betaPos2) {
-                    // one is beta, but the other not, if the non-beta has more elements, it is actually higher
-                    if(parts1.length <= parts2.length) {
-                        return -1;
-                    }
-                    // first is beta, second not, but they were equal up to now
-                    // indicate that the second should come first
-                    return 1;
-                } else if (!betaPos1 && betaPos2) {
-                    // one is beta, but the other not, if the non-beta has more elements, it is actually higher
-                    if(parts1.length >= parts2.length) {
-                        return 1;
-                    }
-                    // first is normal, second is beta, but they were equal up to now
-                    // indicate that the first should come first
-                    return -1;
-                }
-
-                int ret = Integer.compare(Integer.parseInt(part1), Integer.parseInt(part2));
-                if (ret != 0) {
-                    return ret;
-                }
+            // if the parsed version is equal, we still want to compare the string
+            // itself as there can be subtle differences, e.g. "3.9" and "3.9.0" and
+            // we still want to keep these versions apart in sorted sets
+            if(ret == 0) {
+                return var1.compareTo(var2);
             }
 
-            // at this point we had equal numbers for the number of items that both have in common
-            if(parts1.length != parts2.length) {
-                if (beta1 && !beta2) {
-                    return -1;
-                } else if (beta2 && !beta1) {
-                    return 1;
-                }
-
-                // otherwise the longer one is a higher version, e.g. 3.10 and 3.10.1
-                return parts1.length < parts2.length ? -1 : 1;
-            }
+            return ret;
         }
 
         // finally resort to normal textual comparison
@@ -86,5 +169,13 @@ public class VersionComparator implements Comparator<String> {
             return 1;
         }
         return var1.compareTo(var2);
+    }
+
+    private static boolean isBeta(String part) {
+        return StringUtils.startsWithIgnoreCase(part, "BETA");
+    }
+
+    private static boolean isSnapshot(String part) {
+        return StringUtils.startsWithIgnoreCase(part, "SNAPSHOT");
     }
 }
--- src/test/java/org/dstadler/github/VersionComparatorTest.java
+++ src/test/java/org/dstadler/github/VersionComparatorTest.java
@@ -3,10 +3,10 @@ package org.dstadler.github;
 import org.dstadler.commons.testing.TestHelpers;
 import org.junit.Test;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class VersionComparatorTest {
     @SuppressWarnings("RedundantStringFormatCall")
@@ -16,7 +16,10 @@ public class VersionComparatorTest {
         TestHelpers.ComparatorTest(new VersionComparator(), "1.0", String.format("1.0"), "1.1", false);
         TestHelpers.ComparatorTest(new VersionComparator(), "1.0", String.format("1.0"), "0.9", true);
         TestHelpers.ComparatorTest(new VersionComparator(), "1.0", String.format("1.0"), "1.0-beta1", true);
+        TestHelpers.ComparatorTest(new VersionComparator(), "1.0", String.format("1.0"), "1.0-BETA1", true);
+        TestHelpers.ComparatorTest(new VersionComparator(), "1.0", String.format("1.0"), "1.0-SNAPSHOT", true);
         TestHelpers.ComparatorTest(new VersionComparator(), "1.0-beta1", String.format("1.0-beta1"), "1.0", false);
+        TestHelpers.ComparatorTest(new VersionComparator(), "1.0-beta1", String.format("1.0-beta1"), "1.0-SNAPSHOT", true);
         TestHelpers.ComparatorTest(new VersionComparator(), "3.10", String.format("3.10"), "3.10.1", false);
         TestHelpers.ComparatorTest(new VersionComparator(), "3.10-beta1", String.format("3.10-beta1"), "3.10.1", false);
         TestHelpers.ComparatorTest(new VersionComparator(), "3.10-beta1", String.format("3.10-beta1"), "3.10.1.2", false);
@@ -27,55 +30,274 @@ public class VersionComparatorTest {
 
         // 'other' currently is sorted higher than numbers
         TestHelpers.ComparatorTest(new VersionComparator(), "1.0", String.format("1.0"), "other", false);
+
+        // arbitrary text is still sorted as good as possible
+        TestHelpers.ComparatorTest(new VersionComparator(), "blabla1", String.format("blabla1"), "blabla2", false);
     }
 
     @Test
     public void testSortedSet() {
-        Set<String> set = new TreeSet<>(new VersionComparator());
-        set.add("other");
-        set.add("3.15");
-        set.add("3.15-beta2");
-        set.add("3.15-beta1");
-        set.add("3.14");
-        set.add("3.14-beta2");
-        set.add("3.14-beta1");
-        set.add("3.13");
-        set.add("3.13-beta1");
-        set.add("3.12");
-        set.add("3.12-beta1");
-        set.add("3.11");
-        set.add("3.10");
-        set.add("3.10-beta2");
-        set.add("3.10-beta1");
-        set.add("3.9.0");
-        set.add("3.9");
-        set.add("3.8");
-        set.add("3.8-beta5");
-        set.add("3.8-beta4");
-        set.add("3.8-beta3");
-        set.add("3.8-beta2");
-        set.add("3.7");
-        set.add("3.7-beta3");
-        set.add("3.6");
-        set.add("3.5-beta5");
-        set.add("3.5-beta3");
-        set.add("3.2");
-        set.add("3.14-beta1-20151223");
-        set.add("3.13-SNAPSHOT");
-        set.add("3.12-bbn");
-        set.add("3.12-SNAPSHOT");
-        set.add("3.11-beta3");
-        set.add("3.11-beta2");
-        set.add("3.11-BETA3");
-        set.add("3.10.1");
-        set.add("3.0.1");
-        set.add("3.0");
+        Set<String> list = new TreeSet<>(new VersionComparator());
+        list.add("other");
+        list.add("3.15");
+        list.add("3.15-beta2");
+        list.add("3.15-beta1");
+        list.add("3.14");
+        list.add("3.14-beta2");
+        list.add("3.14-beta1");
+        list.add("3.13");
+        list.add("3.13-beta1");
+        list.add("3.12");
+        list.add("3.12-beta1");
+        list.add("3.11");
+        list.add("3.10");
+        list.add("3.10-beta2");
+        list.add("3.10-beta1");
+        list.add("3.9.0");
+        list.add("3.9");
+        list.add("3.8");
+        list.add("3.8-beta5");
+        list.add("3.8-beta4");
+        list.add("3.8-beta3");
+        list.add("3.8-beta2");
+        list.add("3.7");
+        list.add("3.7-beta3");
+        list.add("3.6");
+        list.add("3.5-beta5");
+        list.add("3.5-beta3");
+        list.add("3.2");
+        list.add("3.14-beta1-20151223");
+        list.add("3.13-SNAPSHOT");
+        list.add("3.12-bbn");
+        list.add("3.12-SNAPSHOT");
+        list.add("3.11-beta3");
+        list.add("3.11-beta2");
+        list.add("3.11-BETA3");
+        list.add("3.10.1");
+        list.add("3.0.1");
+        list.add("3.0");
 
-        assertEquals("[3.0, 3.0.1, 3.11-BETA3, 3.12-SNAPSHOT, 3.12-bbn, 3.2, 3.5-beta3, 3.5-beta5, 3.6, " +
+        assertEquals("[3.0, 3.0.1, 3.2, 3.5-beta3, 3.5-beta5, 3.6, " +
                         "3.7-beta3, 3.7, 3.8-beta2, 3.8-beta3, 3.8-beta4, 3.8-beta5, 3.8, 3.9, 3.9.0, " +
-                        "3.10-beta1, 3.10-beta2, 3.10, 3.10.1, 3.11-beta2, 3.11-beta3, 3.11, 3.12-beta1, 3.12, " +
-                        "3.13-beta1, 3.13, 3.13-SNAPSHOT, 3.14-beta1, 3.14-beta1-20151223, 3.14-beta2, 3.14, " +
+                        "3.10-beta1, 3.10-beta2, 3.10, 3.10.1, 3.11-beta2, 3.11-BETA3, 3.11-beta3, 3.11, 3.12-SNAPSHOT, 3.12-beta1, 3.12, 3.12-bbn, " +
+                        "3.13-SNAPSHOT, 3.13-beta1, 3.13, 3.14-beta1, 3.14-beta1-20151223, 3.14-beta2, 3.14, " +
                         "3.15-beta1, 3.15-beta2, 3.15, other]",
-                set.toString());
+                list.toString());
+    }
+
+    @Test
+    public void testSortedListReverseSortedAdd() {
+        List<String> list = new ArrayList<>();
+        list.add("other");
+        list.add("3.15");
+        list.add("3.15-beta2");
+        list.add("3.15-beta1");
+        list.add("3.14");
+        list.add("3.14-beta2");
+        list.add("3.14-beta1");
+        list.add("3.13");
+        list.add("3.13-beta1");
+        list.add("3.12");
+        list.add("3.12-beta1");
+        list.add("3.11");
+        list.add("3.10");
+        list.add("3.10-beta2");
+        list.add("3.10-beta1");
+        list.add("3.9.0");
+        list.add("3.9");
+        list.add("3.8");
+        list.add("3.8-beta5");
+        list.add("3.8-beta4");
+        list.add("3.8-beta3");
+        list.add("3.8-beta2");
+        list.add("3.7");
+        list.add("3.7-beta3");
+        list.add("3.6");
+        list.add("3.5-beta5");
+        list.add("3.5-beta3");
+        list.add("3.2");
+        list.add("3.14-beta1-20151223");
+        list.add("3.13-SNAPSHOT");
+        list.add("3.12-bbn");
+        list.add("3.12-SNAPSHOT");
+        list.add("3.11-beta3");
+        list.add("3.11-beta2");
+        list.add("3.11-BETA3");
+        list.add("3.10.1");
+        list.add("3.0.1");
+        list.add("3.0");
+
+        Collections.sort(list, new VersionComparator());
+
+        assertEquals("[3.0, 3.0.1, 3.2, 3.5-beta3, 3.5-beta5, 3.6, " +
+                        "3.7-beta3, 3.7, 3.8-beta2, 3.8-beta3, 3.8-beta4, 3.8-beta5, 3.8, 3.9, 3.9.0, " +
+                        "3.10-beta1, 3.10-beta2, 3.10, 3.10.1, 3.11-beta2, 3.11-BETA3, 3.11-beta3, 3.11, 3.12-SNAPSHOT, 3.12-beta1, 3.12, 3.12-bbn, " +
+                        "3.13-SNAPSHOT, 3.13-beta1, 3.13, 3.14-beta1, 3.14-beta1-20151223, 3.14-beta2, 3.14, " +
+                        "3.15-beta1, 3.15-beta2, 3.15, other]",
+                list.toString());
+    }
+
+    @Test
+    public void testSortedListArbitraryAdd() {
+        List<String> list = new ArrayList<>();
+        list.add("3.11-BETA3");
+        list.add("3.8-beta3");
+        list.add("3.11-beta2");
+        list.add("3.14-beta1");
+        list.add("3.13");
+        list.add("3.13-beta1");
+        list.add("3.12");
+        list.add("3.15");
+        list.add("3.12-beta1");
+        list.add("3.10.1");
+        list.add("3.10");
+        list.add("3.10-beta2");
+        list.add("3.0.1");
+        list.add("3.5-beta3");
+        list.add("3.9.0");
+        list.add("3.9");
+        list.add("3.8-beta4");
+        list.add("3.8");
+        list.add("3.0");
+        list.add("3.8-beta5");
+        list.add("3.8-beta2");
+        list.add("3.7");
+        list.add("3.7-beta3");
+        list.add("3.12-SNAPSHOT");
+        list.add("3.6");
+        list.add("3.14");
+        list.add("3.15-beta2");
+        list.add("3.15-beta1");
+        list.add("3.5-beta5");
+        list.add("3.2");
+        list.add("3.14-beta1-20151223");
+        list.add("3.13-SNAPSHOT");
+        list.add("3.10-beta1");
+        list.add("3.14-beta2");
+        list.add("3.12-bbn");
+        list.add("3.11");
+        list.add("other");
+        list.add("3.11-beta3");
+
+        Collections.sort(list, new VersionComparator());
+
+        assertEquals("[3.0, 3.0.1, 3.2, 3.5-beta3, 3.5-beta5, 3.6, " +
+                        "3.7-beta3, 3.7, 3.8-beta2, 3.8-beta3, 3.8-beta4, 3.8-beta5, 3.8, 3.9, 3.9.0, " +
+                        "3.10-beta1, 3.10-beta2, 3.10, 3.10.1, 3.11-beta2, 3.11-BETA3, 3.11-beta3, 3.11, 3.12-SNAPSHOT, 3.12-bbn, 3.12-beta1, 3.12, " +
+                        "3.13-SNAPSHOT, 3.13-beta1, 3.13, 3.14-beta1, 3.14-beta1-20151223, 3.14-beta2, 3.14, " +
+                        "3.15-beta1, 3.15-beta2, 3.15, other]",
+                list.toString());
+    }
+
+    @Test
+    public void testParse() {
+        assertEquals("Version{major=1, minor=0, revision=0, beta=false, snapshot=false, other=false, betaSnapshotOther=0}", VersionComparator.Version.parse("1.0").toString());
+        assertEquals("Version{major=3, minor=0, revision=1, beta=false, snapshot=false, other=false, betaSnapshotOther=0}", VersionComparator.Version.parse("3.0.1").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=true, snapshot=false, other=false, betaSnapshotOther=0}", VersionComparator.Version.parse("3.5-beta").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=true, snapshot=false, other=false, betaSnapshotOther=2}", VersionComparator.Version.parse("3.5-beta2").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=true, snapshot=false, other=false, betaSnapshotOther=2}", VersionComparator.Version.parse("3.5-BETA2").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=false, snapshot=true, other=false, betaSnapshotOther=0}", VersionComparator.Version.parse("3.5-SNAPSHOT").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=false, snapshot=true, other=false, betaSnapshotOther=17}", VersionComparator.Version.parse("3.5-SNAPSHOT17").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=false, snapshot=false, other=true, betaSnapshotOther=0}", VersionComparator.Version.parse("3.5-bbn").toString());
+        assertEquals("Version{major=3, minor=5, revision=0, beta=true, snapshot=false, other=false, betaSnapshotOther=20151223}", VersionComparator.Version.parse("3.5-beta-20151223").toString());
+        assertEquals("Version{major=3, minor=14, revision=0, beta=true, snapshot=false, other=false, betaSnapshotOther=1}", VersionComparator.Version.parse("3.14-beta1-20151223").toString());
+        assertEquals("Version{major=3, minor=9, revision=0, beta=false, snapshot=false, other=false, betaSnapshotOther=0}", VersionComparator.Version.parse("3.9.0").toString());
+        assertEquals("Version{major=99, minor=0, revision=0, beta=false, snapshot=false, other=false, betaSnapshotOther=0}", VersionComparator.Version.parse("other").toString());
+    }
+
+    @Test
+    public void testFailingCase1() {
+        VersionComparator.Version v1 = VersionComparator.Version.parse("1.0-SNAPSHOT");
+        VersionComparator.Version v2 = VersionComparator.Version.parse("1.0");
+
+        assertTrue(v1.compareTo(v1) == 0);
+        assertTrue(v2.compareTo(v2) == 0);
+
+        assertTrue(v1.compareTo(v2) < 0);
+        assertTrue(v2.compareTo(v1) > 0);
+
+        final int ret = new VersionComparator().compare("1.0-SNAPSHOT", "1.0");
+        assertTrue("Item 'notequal' should be less than item 'equal' in ComparatorTest, but compare was: " + ret,
+                ret < 0);
+    }
+
+    @Test
+    public void testFailingCase2() {
+        VersionComparator.Version v1 = VersionComparator.Version.parse("3.0.1");
+        VersionComparator.Version v2 = VersionComparator.Version.parse("3.12-SNAPSHOT");
+
+        assertTrue(v1.compareTo(v1) == 0);
+        assertTrue(v2.compareTo(v2) == 0);
+
+        assertTrue(v1.compareTo(v2) < 0);
+        assertTrue(v2.compareTo(v1) > 0);
+
+        final int ret = new VersionComparator().compare("3.0.1", "3.12-SNAPSHOT");
+        assertTrue("Item 'notequal' should be less than item 'equal' in ComparatorTest, but compare was: " + ret,
+                ret < 0);
+    }
+
+    @Test
+    public void testFailingCase3() {
+        VersionComparator.Version v1 = VersionComparator.Version.parse("3.2");
+        VersionComparator.Version v2 = VersionComparator.Version.parse("3.8-beta5");
+
+        assertTrue(v1.compareTo(v1) == 0);
+        assertTrue(v2.compareTo(v2) == 0);
+
+        assertTrue(v1.compareTo(v2) < 0);
+        assertTrue(v2.compareTo(v1) > 0);
+
+        final int ret = new VersionComparator().compare("3.2", "3.8-beta5");
+        assertTrue("Item 'notequal' should be less than item 'equal' in ComparatorTest, but compare was: " + ret,
+                ret < 0);
+    }
+
+    @Test
+    public void testFailingCase4() {
+        VersionComparator.Version v1 = VersionComparator.Version.parse("3.2-beta");
+        VersionComparator.Version v2 = VersionComparator.Version.parse("3.9");
+
+        assertTrue(v1.compareTo(v1) == 0);
+        assertTrue(v2.compareTo(v2) == 0);
+
+        assertTrue(v1.compareTo(v2) < 0);
+        assertTrue(v2.compareTo(v1) > 0);
+
+        final int ret = new VersionComparator().compare("3.2-beta", "3.9");
+        assertTrue("Item 'notequal' should be less than item 'equal' in ComparatorTest, but compare was: " + ret,
+                ret < 0);
+    }
+
+    @Test
+    public void testFailingCase5() {
+        VersionComparator.Version v1 = VersionComparator.Version.parse("3.2");
+        VersionComparator.Version v2 = VersionComparator.Version.parse("3.14-beta1-20151223");
+
+        assertTrue(v1.compareTo(v1) == 0);
+        assertTrue(v2.compareTo(v2) == 0);
+
+        assertTrue(v1.compareTo(v2) < 0);
+        assertTrue(v2.compareTo(v1) > 0);
+
+        final int ret = new VersionComparator().compare("3.2", "3.14-beta1-20151223");
+        assertTrue("Item 'notequal' should be less than item 'equal' in ComparatorTest, but compare was: " + ret,
+                ret < 0);
+    }
+
+    @Test
+    public void testFailingCase6() {
+        VersionComparator.Version v1 = VersionComparator.Version.parse("3.14-beta1-20151223");
+        VersionComparator.Version v2 = VersionComparator.Version.parse("3.14-beta2");
+
+        assertTrue(v1.compareTo(v1) == 0);
+        assertTrue(v2.compareTo(v2) == 0);
+
+        assertTrue(v1.compareTo(v2) < 0);
+        assertTrue(v2.compareTo(v1) > 0);
+
+        final int ret = new VersionComparator().compare("3.14-beta1-20151223", "3.14-beta2");
+        assertTrue("Item 'notequal' should be less than item 'equal' in ComparatorTest, but compare was: " + ret,
+                ret < 0);
     }
 }
