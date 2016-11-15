70ec4573d11bcdd8fd217f74cbc5b18bb1395915
Dominik Stadler <dominik.stadler@gmx.at>
2016-11-15 06:42:15 +0100

    Adjust to look for Jekyll in the home-dir as well (for Cygwin/manual installation)

--- jekyll.sh
+++ jekyll.sh
@@ -1,1 +1,8 @@
-jekyll build --source docs --destination build/jekyll --watch
+if [ -f ~/bin/jekyll ]
+then
+    JEKYLL=~/bin/jekyll
+else
+    JEKYLL=jekyll
+fi
+
+$JEKYLL build --source docs --destination build/jekyll --watch
